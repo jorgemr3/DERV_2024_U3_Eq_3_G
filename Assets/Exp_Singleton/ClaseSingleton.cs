84254c91420d70505bb666197eec92de86055629
jorgemr3 <a2213332183@alumnos.uat.edu.mx>
2024-10-02 16:45:17 -0600

    agregado librerias tmp de canvas y singleton + managerUI

---
+++ Assets/Exp_Singleton/ClaseSingleton.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClaseSingleton : MonoBehaviour
+{
+    public static ClaseSingleton Instance{get; private set;}  
+    
+
+    private void Awake(){
+        if(Instance != null && Instance != this && this != null){
+            //instance diferente de null
+            //this diferente de instance 
+            //ololllll
+            Destroy(this);
+        }else{
+            DontDestroyOnLoad(gameObject);
+             Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)){
+            int idx_escena = SceneManager.GetActiveScene().buildIndex;
+            idx_escena++;
+            idx_escena%=3;   //total de escenas
+            cambioEscena(idx_escena);
+        }
+       
+    }
+
+    public void cambioEscena(int index){
+        SceneManager.LoadScene(index);
+    }
+}
---
+++ Assets/Exp_Singleton/ManagerUI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ManagerUI : MonoBehaviour
+{
+    float tiempo_transcurrido;
+    float con_segundos;
+    TextMeshProUGUI []contador;
+
+    public static ManagerUI Instance{get; private set;}  
+    private void Awake(){
+         if(Instance != null && Instance != this ){
+            //instance diferente de null
+            //this diferente de instance 
+            Destroy(this);
+        }else{
+            contador = GetComponentsInChildren<TextMeshProUGUI>();
+            Instance = this;
+        }
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        con_segundos = 0;  
+        tiempo_transcurrido =0;     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo_transcurrido +=Time.deltaTime;
+        if(tiempo_transcurrido > 1.0f){
+            con_segundos++;
+            tiempo_transcurrido =0;
+            contador[0].text = con_segundos.ToString();
+        }    
+    }
+}
